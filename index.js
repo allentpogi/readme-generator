6a22b8ea62eebc5afa5f969e3538c0ad11d1ffc1
allen <allen.tuazon@gmail.com>
2023-04-21 23:46:34 +1000

    updating the questions array

--- index.js
+++ index.js
@@ -12,43 +12,48 @@ const questions = [
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a brief description of your project:',
+      message: 'Provide a short description explaining the what, why, and how of your project.',
     },
     {
       type: 'input',
       name: 'installation',
-      message: 'What are the installation instructions for your project?',
+      message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
     },
     {
       type: 'input',
       name: 'usage',
-      message: 'What is the usage information for your project?',
+      message: 'Provide instructions and examples for use. Include screenshots as needed.',
     },
     {
       type: 'input',
-      name: 'contributing',
-      message: 'What are the contributing guidelines for your project?',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'What are the test instructions for your project?',
+      name: 'credits',
+      message: 'List your collaborators, if any, with links to their GitHub profiles.',
     },
     {
       type: 'list',
       name: 'license',
       message: 'Choose a license for your project:',
-      choices: ['MIT', 'GNU GPL v3', 'Apache 2.0', 'BSD 3-Clause'],
+      choices: ['Apache 2.0', 'BSD 3-Clause', 'GNU GPL v3', 'ISC', 'MIT'],
     },
     {
       type: 'input',
-      name: 'github',
-      message: 'What is your GitHub username?',
+      name: 'badges',
+      message: 'Include your badge, if any.',
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'If your project has a lot of features, list them here.',
     },
     {
       type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
+      name: 'contribute',
+      message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions for your project?',
     },
 ];
 
@@ -62,47 +67,3 @@ function init() {}
 init();
 
 
-
-
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-inquirer
-  .prompt()
-  .then((answers) => {
-    const readmeTemplate = `
-# ${answers.title}
-
-${answers.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [License](#license)
-- [Questions](#questions)
-
-## Installation
-${answers.installation}
-
-## Usage
-${answers.usage}
-
-## Contributing
-${answers.contributing}
-
-## Tests
-${answers.tests}
-
-## License
-This project is licensed under the ${answers.license} License.
-
-## Questions
-Please contact me at [${answers.email}](mailto:${answers.email}) if you have any questions. You can also find me on GitHub at [${answers.github}](https://github.com/${answers.github}/).
-
-`;
-    fs.writeFile('README.md', readmeTemplate, (err) =>
-      err ? console.error(err) : console.log('README.md generated!')
-    );
-  });

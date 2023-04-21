86e9a63e3bc54f46177774e9eb72ef537f5059c8
allen <allen.tuazon@gmail.com>
2023-04-21 23:36:24 +1000

    making some progress. added the questions in an array

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information for your project?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contributing guidelines for your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions for your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GNU GPL v3', 'Apache 2.0', 'BSD 3-Clause'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +60,49 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt()
+  .then((answers) => {
+    const readmeTemplate = `
+# ${answers.title}
+
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} License.
+
+## Questions
+Please contact me at [${answers.email}](mailto:${answers.email}) if you have any questions. You can also find me on GitHub at [${answers.github}](https://github.com/${answers.github}/).
+
+`;
+    fs.writeFile('README.md', readmeTemplate, (err) =>
+      err ? console.error(err) : console.log('README.md generated!')
+    );
+  });
